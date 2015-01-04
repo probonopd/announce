70ff939f60d86759cb04d67bfe2e4a61c39d3c05
probonopd <probono@puredarwin.org>
2015-01-04 18:58:26 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -40,7 +40,11 @@ EOF
 git add .
 git pull
 git commit -a -m "Deploy packages to gh-pages branch"
-find .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+echo -e "Pushing to origin/gh-pages"
+git push -fq origin gh-pages > /dev/null 2>&1
+echo -e "Uploaded generated files to gh-pages\n"
+# find .
 # 2>&1 so that the key does not leak to the logs in case of errors
-git push --force --quiet https://${TOKEN}@github.com/${USER}/${REPO}.git master:gh-pages # > /dev/null 2>&1 || exit 1
+# git push --force --quiet https://${TOKEN}@github.com/${USER}/${REPO}.git master:gh-pages # > /dev/null 2>&1 || exit 1
 cd -
